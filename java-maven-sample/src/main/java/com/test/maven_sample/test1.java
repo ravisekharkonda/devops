0489eea544003869a61d0161bccde7cffdec0331
ravkonda <ravkonda@RAVKONDA-IN.oradev.oraclecorp.com>
2019-07-17 11:58:57 +0530

    adding java-maven-sample

---
+++ java-maven-sample/src/main/java/com/test/maven_sample/test1.java
@@ -0,0 +1,15 @@
+package com.test.maven_sample;
+
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
+/**
+ * Sample code that uses JavaParser.
+ */
+public class test1 {
+    public static void main(String[] args) throws IOException 
+	{
+        System.out.println ( "Hello Jenkins");   
+    }
+}
